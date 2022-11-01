f9fa30d9ce65ec1065875c423711fee27df252fa
merryklawrel <merrylawrel@gmail.com>
2022-11-01 18:43:11 +0530

    error correction made

--- apps.py
+++ apps.py
@@ -7,6 +7,8 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
+from db import db
+  
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -22,6 +24,5 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-  from db import db
   db.init_app(app)  
   app.run(port=5000,debug=True)
