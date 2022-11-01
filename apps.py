89b5665308f0689706d9a63d1db29dec44f6cfe4
merryklawrel <merrylawrel@gmail.com>
2022-11-01 19:19:01 +0530

    correction made

--- apps.py
+++ apps.py
@@ -6,7 +6,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
 from db import db
   
 app = Flask(__name__)
