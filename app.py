4290ce71fe57b6bdc3c9ec4ace7a8cf61e5d67de
merryklawrel <merrylawrel@gmail.com>
2022-11-07 13:30:29 +0530

    Modification

--- app.py
+++ app.py
@@ -22,5 +22,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
+  db.init_app(app)
   app.run(port=5000,debug=True)
