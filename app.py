74ba2ac1921dcaaf5463d6f3a5b0e7a480077383
merryklawrel <merrylawrel@gmail.com>
2022-11-16 17:47:18 +0530

    libraries updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
-  
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
